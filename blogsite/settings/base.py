3656f88ae51abba67b1db1e517365b9ba10830e4
Chirag Kanzariya <chirag9587@gmail.com>
2019-07-01 13:51:47 +0530

    change base and production

--- blogsite/settings/base.py
+++ blogsite/settings/base.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blogsite',
     }
 }
